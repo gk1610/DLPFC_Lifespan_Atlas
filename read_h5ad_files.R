9deff03ed82ab92ba3f2a465b96441c623d930a0
gk1610 <84088147+gk1610@users.noreply.github.com>
2023-08-03 14:47:14 -0400

    Create psuedobulk_minerva_paths.R

---
+++ read_h5ad_files.R
@@ -0,0 +1,18 @@
+
+### this is our pseudobulk files to be used for any downstream analysis
+### no duplicates matrices
+### class
+/sc/arion/projects/psychAD/NPS-AD/freeze2_rc/pseudobulk/AGING_2023-06-09_01_45_PB_SubID_bulk.RDS
+### subtype
+/sc/arion/projects/psychAD/NPS-AD/freeze2_rc/pseudobulk/AGING_2023-06-09_01_45_PB_SubID_subtype.RDS
+### sublcass
+/sc/arion/projects/psychAD/NPS-AD/freeze2_rc/pseudobulk/AGING_2023-06-09_01_45_PB_SubID_subclass.RDS
+
+### with duplicates matrices
+### class
+/sc/arion/projects/psychAD/NPS-AD/freeze2_rc/pseudobulk/AGING_2023-06-09_01_45_PB_Channel_bulk.RDS
+### subtype
+/sc/arion/projects/psychAD/NPS-AD/freeze2_rc/pseudobulk/AGING_2023-06-09_01_45_PB_Channel_subtype.RDS
+### sublcass
+/sc/arion/projects/psychAD/NPS-AD/freeze2_rc/pseudobulk/AGING_2023-06-09_01_45_PB_Channel_subclass.RDS
+
