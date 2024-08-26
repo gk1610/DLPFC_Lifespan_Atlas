c3f02a664d6cc1124d63fa59b7ccf06d72238f79
gk1610 <84088147+gk1610@users.noreply.github.com>
2024-08-26 18:30:29 -0400

    Update 5_mean_expression_trajectory_per_subclass.R

--- lifespan_trends/5_mean_expression_trajectory_per_subclass.R
+++ lifespan_trends/5_mean_expression_trajectory_per_subclass.R
@@ -26,18 +26,28 @@ library(gridExtra)
 library(aplot)
 library(ggtree)})
 
+good_plot=function(g1,sz,rot){
+g1=g1+theme_classic()+theme(axis.title= element_text(color="black", size=sz),axis.text = element_text(color="black", size=sz),axis.text.x=element_text(angle=rot,hjust=1,vjust=1))
+g1=g1+theme(legend.position="right")+theme(strip.text=element_text(face="bold",color="black"),strip.background=element_rect(fill="#eeeeee",color="#eeeeee"))
+g1+theme(axis.title= element_text(color="black", size=sz),axis.text = element_text(color="black", size=sz))+theme(aspect.ratio=1)+theme(panel.border = element_rect(size = 1, color = "black", fill = NA))
+}
+
 best_model_list=list("poly_with_two_df_log_Age"= "~ 0 + poly(log2(Age+1),df=2)")
 
+### this is the list of coefs from best model 
 load(paste0("lifespan_trends/best_model/lifespan_coefs_dream_results_best_model.RDATA"))
 lifespan_dream_results_df$celltype_ID=paste0(lifespan_dream_results_df$celltype,"_",lifespan_dream_results_df$ID)
 
-clusters_df=read.csv("lifespan_trends/best_model/clustered_data_nclust10.csv)
-clusters_df$cluster=as.numeric(as.character(clusters_df$cluster))
+### this is the file which has clusters assigned to the coefs from subclass_genes saved as celltype_ID
+clusters_df=read.csv("lifespan_trends/best_model/clustered_data_nclust10.csv")
+clusters_df$cluster=as.numeric(as.character(clusters_df$cluster))+1
 clusters_df$ID=sub(".*_", "", clusters_df$celltype_ID)
 
+### here dreamlet coefs from best model are merged with cluster ids and filtered for which we have FDR<.05
 clusters_lifespan_dream_coefs_df=merge(lifespan_dream_results_df,clusters_df[,c("celltype_ID","cluster")],by='celltype_ID')
 clusters_lifespan_dream_coefs_df=clusters_lifespan_dream_coefs_df[clusters_lifespan_dream_coefs_df$adj.P.Val<.05,]
 
+### now we take residuals which has all the covars regressed out to fit best model and obtain lifespan trajectory per cluster per subclass
 
 load(paste0("lifespan_trends/residuals/residuals_subclass.RDATA"))
 metadata$SubID=rownames(metadata)
@@ -46,16 +56,10 @@ subclass_order=c("EN_L6_CT", "EN_L5_6_NP", "EN_L6B", "EN_L3_5_IT_1", "EN_L3_5_IT
     "EN_L6_IT_2","IN_LAMP5_RELN", "IN_LAMP5_LHX6","IN_ADARB2","IN_PVALB_CHC", "IN_PVALB", "IN_SST", "IN_VIP",
     "Astro","Oligo","OPC","Micro","Adaptive", "PVM","SMC","VLMC", "Endo","PC")
 
-average_model_list=list("poly_with_two_df_log_Age"= "~ 0 + poly(log2(Age+1),df=2)")
+model_list=list("poly_with_two_df_log_Age"= "~ 0 + poly(log2(Age+1),df=2)")
 
 aging_average_expression=list()
 ct = 1
-
-file_name=gsub('.csv','.pdf',input_file)
-
-pdf(file_name,width=15)
-
-ggplot(clusters_lifespan_dream_coefs_df,aes(coef_1,coef_2,color=factor(cluster)))+geom_point()+theme_bw()
 
 for (mm in (1:max(clusters_lifespan_dream_coefs_df$cluster))) {
 
@@ -64,32 +68,37 @@ celltypes=unique(clusters_lifespan_dream_coefs_df$celltype[clusters_lifespan_dre
 
 for (kk in (1:length(celltypes))) {
 
-print(mm)
+input_genes=clusters_lifespan_dream_coefs_df$ID[clusters_lifespan_dream_coefs_df$cluster==mm & clusters_lifespan_dream_coefs_df$celltype == celltypes[kk]]
 
-input_gene=clusters_lifespan_dream_coefs_df$ID[clusters_lifespan_dream_coefs_df$cluster==mm & clusters_lifespan_dream_coefs_df$celltype == celltypes[kk]]
-
-if (length(input_gene) >1) {
-coefs=clusters_lifespan_dream_coefs_df[clusters_lifespan_dream_coefs_df$cluster==mm & clusters_lifespan_dream_coefs_df$celltype == celltypes[kk],c("coef_1","coef_2")]
-coefs_mean=apply(coefs,2,mean)
-print(length(input_gene))
-
+## first we get residuals of celltypes[kk] and subset it for input_gene
+    
 res_mat_subset=res_mat[[which(names(res_mat) %in% celltypes[kk])]]
-res_mat_subset_melted=melt(res_mat_subset[rownames(res_mat_subset) %in% input_gene,])
+res_mat_subset_melted=melt(res_mat_subset[rownames(res_mat_subset) %in% input_genes,])
 colnames(res_mat_subset_melted)[1:3]=c("ID","SubID","value")
 data_expression_Age=merge(res_mat_subset_melted,metadata[,c("SubID","Age")],by="SubID")
 
+if (length(input_gene) >1) {
+coefs=clusters_lifespan_dream_coefs_df[clusters_lifespan_dream_coefs_df$cluster==mm & clusters_lifespan_dream_coefs_df$celltype == celltypes[kk],c("coef_1","coef_2")]
+
+### get mean of all coefs across a cluster and a celltype
+coefs_mean=apply(coefs,2,mean)
+
+## get mean residualized expression of all genes per donor
 df=data_expression_Age %>% dplyr::group_by(SubID)%>% dplyr::summarize(mean_expr=mean(value),Age=mean(Age))
-fit=lm(paste0('mean_expr',average_model_list[["poly_with_two_df_log_Age"]]),data=df)
+
+## fit the mean residualized expr to the nonlinear age trend from polynomial model
+    
+fit=lm(paste0('mean_expr',model_list[["poly_with_two_df_log_Age"]]),data=df)
 df$fitted_line=predict(fit)
-x_axis=model.matrix(as.formula(average_model_list[["poly_with_two_df_log_Age"]]),data=df)
-df$fitted_dream_line=x_axis %*% unlist(coefs_mean)[grep("PMI|n_genes|PC|Intercept", names(unlist(coefs_mean)),invert = TRUE)]
+x_axis=model.matrix(as.formula(model_list[["poly_with_two_df_log_Age"]]),data=df)
+df$fitted_dream_line=x_axis %*% unlist(coefs_mean)[grep("Age", names(unlist(coefs_mean)),invert = TRUE)]
 df$fitted_dream_line_scaled=df$fitted_dream_line-df$fitted_dream_line[which.min(df$Age)]
 df=as.data.frame(df)
 df$celltype=celltypes[kk]
-  df$cluster=mm
+df$cluster=mm
 
 aging_average_expression[[ct]]=df
-aging_data_plot[[kk]]=df
+aging_average_expression_celltype[[kk]]=df
 ct = ct + 1
 
 } else {
@@ -99,21 +108,24 @@ next
 }
 
 }
-aging_data_plot_df=as.data.frame(do.call(rbind,aging_data_plot))
+aging_average_expression_celltype_df=as.data.frame(do.call(rbind,aging_average_expression_celltype))
 
-g1=ggplot(aging_data_plot_df,aes(Age,mean_expr))+geom_point()+geom_line(data=aging_data_plot_df,aes(Age,fitted_dream_line,color=celltype))
-print(good_plot(g1,12,0)+ggtitle(paste0("cluster_",mm))+scale_color_manual(values=subclass_color_map))
-
-g1=ggplot(aging_data_plot_df,aes(Age,mean_expr))+geom_line(data=aging_data_plot_df,aes(Age,fitted_dream_line,color=celltype))
-print(good_plot(g1,12,0)+ggtitle(paste0("cluster_",mm))+scale_color_manual(values=subclass_color_map))+ylab("Predicted expression per celltype using mean of clustered coefficients of genes")
+### cell type trajectory per cluster (Supplementary Figure 6b)
+    
+g1=ggplot(aging_average_expression_celltype_df,aes(Age,mean_expr))+geom_point()+geom_line(data=aging_average_expression_celltype_df,aes(Age,fitted_dream_line,color=celltype))
+print(good_plot(g1,12,0)+ggtitle(paste0("cluster_",mm))
 
 }
 
+
 aging_average_expression_df=do.call(rbind,aging_average_expression)
 aging_average_expression_df_summary=aging_average_expression_df %>% dplyr::group_by(cluster,SubID) %>% dplyr::summarize(mean_fitted_dream_line=mean(fitted_dream_line_scaled),Age=mean(Age))
-aging_average_expression_df_summary$cluster=factor(aging_average_expression_df_summary$cluster,levels=cluster_order)
 
-print(ggplot(aging_average_expression_df_summary,aes(Age,mean_fitted_dream_line,color=factor(cluster),group=cluster))+geom_line()+geom_point(color="black")+facet_wrap2(~cluster,ncol=5,strip=strip)+theme_bw()+theme(legend.position="none"))
+### Fig. 2a
+
+pdf(paste0(data_dir"/lifespan_trends.pdf"))
+
+print(ggplot(aging_average_expression_df_summary,aes(Age,mean_fitted_dream_line,color=factor(cluster),group=cluster))+geom_line()+geom_point(color="black")+facet_wrap(~cluster,ncol=5)+theme_bw()+theme(legend.position="none"))
 
 dev.off()
 
