4c6b976cb7b6101fb7c9228631cf8f2c42f66739
gk1610 <84088147+gk1610@users.noreply.github.com>
2024-08-26 13:51:30 -0400

    Update 1_nl_models_testing.R

--- lifespan_trends/1_nl_models_testing.R
+++ lifespan_trends/1_nl_models_testing.R
@@ -38,6 +38,24 @@ g1=g1+theme(legend.position="right")+theme(strip.text=element_text(face="bold",c
 g1+theme(axis.title= element_text(color="black", size=sz),axis.text = element_text(color="black", size=sz))+theme(aspect.ratio=1)+theme(panel.border = element_rect(size = 1, color = "black", fill = NA))
 }
 
+
+add_class2_metadata=function(metadata,col_name){
+class2_order=c("EN","IN","Non_neurons","Adaptive_Endo_Mural")
+metadata$class="Endo_Mural"
+metadata$class[grep("^EN",metadata[,col_name])]="EN"
+metadata$class[grep("^IN",metadata[,col_name])]="IN"
+metadata$class[grep("Astro",metadata[,col_name])]="Non_neurons"
+metadata$class[grep("OPC",metadata[,col_name])]="Non_neurons"
+metadata$class[grep("Olig",metadata[,col_name])]="Non_neurons"
+metadata$class[grep("Micro",metadata[,col_name])]="Non_neurons"
+metadata$class[grep("Endo",metadata[,col_name])]="Adaptive_Endo_Mural"
+metadata$class[grep("^PC|SMC|VLMC|Adaptive|PVM",metadata[,col_name])]="Adaptive_Endo_Mural"
+metadata$class=factor(metadata$class,levels=(class2_order))
+metadata
+
+}
+## code starts here
+
 lifespan_dir="lifespan_trends" 
 dir.create(lifespan_dir)
 
@@ -92,5 +110,23 @@ BIC_list[[ii]]$model_type=gsub("_nl.RDATA","",basename(files[ii]))
 
 save(BIC_list,file=paste0(data_dir,"/BIC_all_models.RDATA"))
 
+
+# BIC
+
+BIC_df=do.call(rbind,BIC_list)
+df=BIC_df %>% group_by(model_type,celltype) %>% summarize(mean_BIC=mean(BIC))
+df=as.data.frame(df)
+df$type="linear"
+df$type[grep("log",df$model_type)]="log"
+df=add_class2_metadata(df,"celltype")
+df$model_type=gsub("poly_with_","",df$model_type)
+df$model_type=factor(df$model_type,levels=c("linear","log_Age","two_df","two_df_log_Age","three_df","three_df_log_Age","four_df","four_df_log_Age","five_df","five_df_log_Age","six_df","six_df_log_Age"))
+
+paste0(data_dir,"/BIC_all_models.pdf",width=15)
+
+g1=ggplot(df,aes(factor(model_type),mean_BIC,color=factor(celltype),group=factor(celltype)))+geom_point()+geom_line()+facet_wrap(~type+class,ncol=4,scale="free")
+good_plot(g1,12,45)+theme(legend.position="none")
+
+dev.off()
 
   
