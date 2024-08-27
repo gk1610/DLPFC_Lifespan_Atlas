dc6e135e747eb59d3af0043e3a42f39bcb0eaaa2
gk1610 <84088147+gk1610@users.noreply.github.com>
2024-08-26 21:11:11 -0400

    Update 2_tau_scores.R

--- degree_of_sharing/2_tau_scores.R
+++ degree_of_sharing/2_tau_scores.R
@@ -1,1 +1,113 @@
+pb=readRDS("/sc/arion/projects/psychAD/NPS-AD/freeze2_rc/pseudobulk/AGING_2024-02-01_22_23_PB_SubID_subclass.RDS")
+colData(pb)$SubID=rownames(colData(pb))
+
+#### keep final samples
+samples_to_keep=read.table("/sc/arion/projects/CommonMind/aging/resources/AGING_2024-02-01_22_23_processAssays_SubID_subclass.txt")
+pb_subset=pb[,colData(pb)$SubID %in% samples_to_keep$V1]
+pb_subset=pb_subset[,colData(pb_subset)$Age<20]
+
+# Stack assays for joint analysis
+pb.stack=stackAssays(pb_subset, assays = grep("^EN",subclass_order,invert=FALSE,value=TRUE))
+
+geneExpr <- assay(pb.stack, 1)
+rownames(geneExpr) <- rownames(pb.stack)
+geneExpr_keep=geneExpr[rownames(geneExpr) %in% PC_genes$gene_name,]
+
+### find counts per million
+idx <- which(colSums(geneExpr_keep, useNames = TRUE) <5)
+
+if (length(idx)>0){
+geneExpr_keep <- geneExpr_keep[, -idx]
+}
+
+y <- DGEList(geneExpr_keep, remove.zeros = TRUE)
+y <- calcNormFactors(y, method = "TMM")
+geneExpr_cluster_cpm <- edgeR::cpm(y, log = FALSE)
+geneExpr_cluster_cpm=as.data.frame(geneExpr_cluster_cpm)
+geneExpr_cluster_cpm$genes=rownames(geneExpr_cluster_cpm)
+
+geneExpr_cluster_cpm_by_donors <- geneExpr_cluster_cpm %>%
+    tidyr::gather(celltype_ID, exp, colnames(select_if(.,is.numeric))) %>% mutate(celltypes=sub("_[^_]+$", "", celltype_ID)) %>%
+    group_by(genes,celltypes) %>% summarize(median_exp=median(exp))
+
+geneExpr_cluster_cpm_by_donors_mat=geneExpr_cluster_cpm_by_donors %>% spread(celltypes, median_exp)
+geneExpr_cluster_cpm_by_donors_mat=as.data.frame(geneExpr_cluster_cpm_by_donors_mat)
+
+#### This step is for finding cell specific scores
+
+num_of_cells <- geneExpr_cluster_cpm_by_donors_mat %>% select_if(is.numeric) %>% names() %>% length()
+result <- geneExpr_cluster_cpm_by_donors_mat %>%
+    tidyr::gather(celltypes, exp, colnames(select_if(.,is.numeric))) %>%   # gather only numeric columns
+    mutate_if(is.numeric, list(~ ifelse(. < 1, 1, .))) %>%  # if there's any expression less than 1, make it 1
+    group_by(genes) %>%                             # calculations will be performed for a gene up to ungroup()
+    mutate(lessthan1 = sum(exp <= 1),
+           lessthan10 = sum(exp <= 10))%>%
+    mutate(log2exp = log2(exp),                             # log transformation is needed for tau calculation
+           sum_log = sum(log2exp),
+           max_log = max(log2exp),
+           tau = (num_of_cells - sum_log/max_log) / (num_of_cells -1))  %>%
+    ungroup() %>%
+    mutate(
+      Status = case_when(
+        lessthan1 == num_of_cells  ~ "Null expresssion",
+        tau < 0.85                   ~ "Wide-spread expression",
+        lessthan10 == num_of_cells ~ "Weak expresssion",
+        TRUE                         ~ "Specific expression"
+      )
+    ) %>%     # distinct(EnsemblGeneID,Status) %>% count(Status) : status counts, see below
+    mutate(tau = ifelse(Status == "Null expresssion", -1, tau)) %>%      # for null exp genes, assign tau as -1
+    select(-(lessthan1:max_log), Tau.Score=tau)  %>%   # remove unnecessary columns before spread
+    spread(celltypes, exp) %>%
+    select(genes, Tau.Score, Status) %>%
+    left_join(geneExpr_cluster_cpm_by_donors_mat, ., by="genes")  # had to use dot since new columns are wanted at the end
+
+library(e1071)
+result_clusters_centers=result %>%
+    select_if(is.numeric) %>%
+    select(-Tau.Score) %>%
+    cmeans(2,iter.max = 100,verbose = FALSE,dist = "euclidean",method = "cmeans",m=2,rate.par = e-10) %>%
+    .[["centers"]] %>% as_tibble()
+
+ result_clusters_centers=result_clusters_centers %<>%
+    rownames_to_column %>%                     # these three lines are just for transposing the data
+    tidyr::gather(var, value, -rowname) %>%    # instead of t(), we preferred
+    spread(rowname, value) %>%                 # tidyverse approach
+    rename(celltypes=var, lower=`1`, upper=`2`)
+
+ geneExpr_cluster_threshold <- geneExpr_cluster_cpm_by_donors_mat %>%
+    tidyr::gather(celltypes, expression, colnames(select_if(.,is.numeric))) %>%
+    left_join(result_clusters_centers, by="celltypes")%>%
+    mutate(clustermean = (lower + upper) /2) %>%    # TODO shouldnt we checking upper level, not the mean?
+    group_by(celltypes) %>%
+    summarize(upper_cluster=mean(upper),          # mean not necessary, just collapsing the repeating data
+              lower_cluster=mean(lower),          # mean not necessary, just collapsing the repeating data
+              average_cluster=mean(clustermean),  # mean not necessary, just collapsing the repeating data
+              no_nonzero=sum(expression > 0),
+              no_elements=sum(expression > clustermean),
+              threshold_ratio=no_elements/no_nonzero)
+
+  threshhold_ratios <- geneExpr_cluster_threshold$threshold_ratio
+  numcells <- length(threshhold_ratios)
+  regressionResult<-lm(threshhold_ratios~seq(numcells))
+  coeffs <- coefficients(regressionResult)
+  optimizedTreshold<-mean(seq(numcells)*coeffs[2]+coeffs[1])
+  tresholdVal<-qnorm(0.9999)- abs(qnorm(optimizedTreshold))
+  result_thresh <- tibble(optimizedTreshold = optimizedTreshold, tresholdVal = tresholdVal)
+  threshold <- result_thresh$tresholdVal
+
+  final_result=geneExpr_cluster_cpm_by_donors_mat %>%
+    tidyr::gather(celltypes, expression, colnames(select_if(.,is.numeric))) %>%   # gather only numeric columns
+    mutate_if(is.numeric, list(~ ifelse(. < 1, 1, .))) %>%  # if there's any expression less than 1, make it 1
+    group_by(genes) %>%
+    mutate(stdDev = sd(expression),
+           maxVal = mean(expression),
+           cutoff = maxVal-stdDev*threshold) %>%
+    dplyr::filter(expression > cutoff) %>%
+    ungroup() %>%
+    select(genes,celltypes,expression,cutoff)
+
+
+save(num_of_cells,final_result,geneExpr_cluster_threshold,result_clusters_centers,result,geneExpr_cluster_cpm_by_donors_mat,geneExpr_cluster_cpm,file="/sc/arion/projects/psychAD/aging/kiran/analysis/lifespan/tauscore_stacked_EN_data_Childhood.RDATA")
+
+
 
