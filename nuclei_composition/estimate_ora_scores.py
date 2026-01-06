fc3942ddd9b7809f5f35e15a18853c96174bbd5b
gk1610 <84088147+gk1610@users.noreply.github.com>
2026-01-06 12:20:19 -0500

    Add ORA scoring for brain-related GO pathways

--- nuclei_composition/estimate_ora_scores.py
+++ nuclei_composition/estimate_ora_scores.py
@@ -1,1 +1,36 @@
+import sys
+import pandas as pd
+import scanpy as sc
+import decoupler as dc
+import numpy as np
+
+# Read Brain related go pathways and corresponding genes list
+# get file from syn72106811 (split this file into chunks for efficiency)
+# syn72106811 are GO pathways that contsins following brain_keywords = c(
+#  "brain", "neuron", "neuro", "glia", "synapse", "axon", 
+#  "dendrite", "astrocyte", "myelin", "cns", "nerve", 
+#  "cortex", "prefrontal", "dlpfc", "pfc", "frontal", 
+#  "microglia", "oligodendrocyte", "excitatory", "inhibitory")
+
+go_bp_df = pd.read_csv(f'brain_related_GO_pathways.csv')
+
+# Read AnnData from syn62147251
+adata = sc.read_h5ad('aging_umap40pca_age_synapse.h5ad')
+
+# Run ORA for chunk
+dc.run_ora(
+    mat=adata,
+    net=go_bp_df,
+    source='geneset',
+    target='genesymbol',
+    verbose=True,
+    use_raw=False
+)
+
+# Save results
+acts = dc.get_acts(adata, obsm_key='ora_estimate')
+acts_v = acts.X.ravel()
+max_e = np.nanmax(acts_v[np.isfinite(acts_v)])
+acts.X[~np.isfinite(acts.X)] = max_e
+pd.DataFrame(acts.obsm['ora_estimate']).to_csv(f'ORA_brain_related_GO_pathways.csv') 
 
